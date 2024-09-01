4912073201ee1cc3476214cc5e3939c17e657d9b
Ike Gabriel Yuson <iggyyuson09@gmail.com>
2024-09-01 21:04:21 +0800

    Initial commit

---
+++ test/amp-and-grafana.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AmpAndGrafana from '../lib/amp-and-grafana-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/amp-and-grafana-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AmpAndGrafana.AmpAndGrafanaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
